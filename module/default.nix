1f0b49350c9abee7020bc0a73b52eff713e78e84
libjared <libjared@users.noreply.github.com>
2022-07-30 16:05:44 -0500

    Add verbose arg to rm

--- module/default.nix
+++ module/default.nix
@@ -212,7 +212,7 @@ in
 
               local dest="$secretOutPath/$name"
 
-              $DRY_RUN_CMD rm -f --interactive=never -- "$dest"
+              $DRY_RUN_CMD rm $VERBOSE_ARG -f --interactive=never -- "$dest"
               $DRY_RUN_CMD ${ageBin} --decrypt ${identityArgs} -o "$dest" "$src"
               $DRY_RUN_CMD chown $VERBOSE_ARG "$owner":"$group" -- "$dest"
               $DRY_RUN_CMD chmod $VERBOSE_ARG "$mode" -- "$dest"
